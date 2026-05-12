a45c8d9a5b37aa977f310e41aaf654cdba38559d
C. Thang Nguyen <thangckt@gmail.com>
2026-05-12 15:00:39 +0900

    Update 0update_spec_version.sh

--- 0update_spec_version.sh
+++ 0update_spec_version.sh
@@ -121,9 +121,9 @@ spec_files="rpm_onlyoffice.spec"
 new_version=$(fetch_github_version "$repo_url")
 update_spec_version "$spec_files" "$new_version" "$store_file"
 
-#####ANCHOR goldendict
-repo_url="https://github.com/goldendict/goldendict"
-spec_files="build_goldendict.spec"
+#####ANCHOR goldendict-ng
+repo_url="https://github.com/xiaoyifang/goldendict-ng"
+spec_files="build_goldendict-ng.spec"
 new_version=$(fetch_github_version "$repo_url")
 update_spec_version "$spec_files" "$new_version" "$store_file"
 
@@ -225,8 +225,8 @@ echo "Update Done!"
 # new_version=$(fetch_gitlab_version "$repo_url")
 # update_spec_version "$spec_files" "$new_version" "$store_file"
 
-#####ANCHOR goldendict-ng
-# repo_url="https://github.com/xiaoyifang/goldendict-ng"
-# spec_files="build_goldendict-ng.spec"
+#####ANCHOR goldendict
+# repo_url="https://github.com/goldendict/goldendict"
+# spec_files="build_goldendict.spec"
 # new_version=$(fetch_github_version "$repo_url")
 # update_spec_version "$spec_files" "$new_version" "$store_file"
